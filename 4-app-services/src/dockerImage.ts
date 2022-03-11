5af9e4d57a711ecc945e95acd27bc35ac3ff6ada
Andrew Meier <ameier38@gmail.com>
2022-03-11 07:17:58 -0500

    add push arg

--- 4-app-services/src/dockerImage.ts
+++ 4-app-services/src/dockerImage.ts
@@ -3,12 +3,13 @@ import * as pulumi from '@pulumi/pulumi'
 import * as path from 'path'
 import * as config from './config'
 
+// NB: must use buildx
 const cloudflaredArm64 = new docker.Image('cloudflared-arm64', {
     imageName: pulumi.interpolate `${config.registryEndpoint}/cloudflared-arm64`,
     build: {
         context: path.join(config.rootDir, 'docker'),
         dockerfile: path.join(config.rootDir, 'docker', 'cloudflared-arm64.Dockerfile'),
-        extraOptions: ['--platform', 'linux/arm64']
+        extraOptions: ['--push', '--platform', 'linux/arm64']
     },
     registry: config.imageRegistry
 })
