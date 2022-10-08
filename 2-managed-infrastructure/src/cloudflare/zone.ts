6f080547c42b24afca5faf4e7b358f061e084ec2
Andrew Meier <ameier38@gmail.com>
2022-10-08 09:23:47 -0400

    add meiermade (#39)
    * add meiermade
    
    * remove import

--- 2-managed-infrastructure/src/cloudflare/zone.ts
+++ 2-managed-infrastructure/src/cloudflare/zone.ts
@@ -13,3 +13,17 @@ new cloudflare.ZoneSettingsOverride('andrewmeier.dev', {
 
 export const andrewmeierDotDevZoneId = andrewmeierDotDev.id
 export const andrewmeierDotDevDomain = andrewmeierDotDev.zone
+
+const meiermadeDotCom = new cloudflare.Zone('meiermade.com', {
+    zone: 'meiermade.com'
+})
+
+new cloudflare.ZoneSettingsOverride('meiermade.com', {
+    zoneId: meiermadeDotCom.id,
+    settings: {
+        ssl: 'strict'
+    }
+})
+
+export const meiermadeDotComZoneId = meiermadeDotCom.id
+export const meiermadeDotComDomain = meiermadeDotCom.zone
