ba93405e164995e3d690e2d91e97ebb97e1131d7
Andrew Meier <ameier38@gmail.com>
2022-10-08 12:08:21 -0400

    update managed infrastructure (#41)

--- 2-managed-infrastructure/src/cloudflare/accessApplication.ts
+++ 2-managed-infrastructure/src/cloudflare/accessApplication.ts
@@ -8,5 +8,6 @@ export const k8sApi = new cloudflare.AccessApplication('k8s-api', {
     accountId: cloudflare.config.accountId,
     allowedIdps: [ githubIdentityProvider.id ],
     autoRedirectToIdentity: true,
-    type: 'self_hosted'
+    type: 'self_hosted',
+    httpOnlyCookieAttribute: true
 })
